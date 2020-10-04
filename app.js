2fbf75479e8f767af68ed90d30f54aa1896fd686
StageLeftHand <69404810+StageLeftHand@users.noreply.github.com>
2020-10-03 17:50:06 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,50 +1,9 @@
-// (function () {
-// 'use strict';
-//
-// angular.module('LunchCheck', [])
-// .controller('LunchCheckController', LunchCheckController);
-//
-// LunchCheckController.$inject = ['$scope']
-// function LunchCheckController($scope) {
-//   $scope.countItems= function(items) {
-//       var count=0;
-// 			if(items==="")
-// 				return 0;
-//       if(items !== undefined){
-// 			var array= items.split(',');
-//     	for (var i = 0; i < array.length; i++) {
-// 			if(array[i].trim()!=="")
-//           count++;
-//     		}
-//       }
-// 			return count;
-// 		};
-//     $scope.showMessage = function () {
-//          var dishCount = $scope.countItems($scope.dishes);
-//          if(dishCount < 1){
-//            $scope.message = "Please enter data first";
-//          }
-//          else if(dishCount <= 3){
-//            $scope.message = "Enjoy";
-//          }
-//          else {
-//            $scope.message = "Too much";
-//          }
-//
-//    };
-//  })();
-
-
-
 (function () {
 'use strict';
-
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
-
 LunchCheckController.$inject = ['$scope']
 function LunchCheckController($scope) {
-  //Function to count number of items from input
   $scope.countItems= function(items) {
 			//, , is not an item
       var count=0;
@@ -59,8 +18,6 @@ function LunchCheckController($scope) {
       }
 			return count;
 		};
-
-  //Function to display appropriate message
     $scope.showMessage = function () {
       var dishCount = $scope.countItems($scope.dishes);
       if(dishCount < 1){
@@ -81,7 +38,6 @@ function LunchCheckController($scope) {
         $scope.applyColor();
       }
   }
-  //Function to apply Color to message and border
    $scope.applyColor = function(){
     $scope.colChange = {
          "color": 'green'
